d90ac8c8234a7ac7d371c654745065cd8fd7a062
Mykyta Vostrikov <87815899+Nick2603@users.noreply.github.com>
2022-06-01 14:54:42 +0300

    Database connected (#1)
    Co-authored-by: Mykyta Vostrikov <mykyta.vostrikov@sigma.software>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'user',
+      database: 'Your_dream_holidays',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
